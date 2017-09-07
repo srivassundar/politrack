e206cb16326058cfe66f4f8d871dd79e9fb40a07
Srivas Sundar <srivas.srihari@gmail.com>
2017-09-07 10:12:38 -0400

    Add option to force update and clean table

--- api-server/scripts/propublica_importer.py
+++ api-server/scripts/propublica_importer.py
@@ -12,6 +12,7 @@ DEFAULTS = {
     'num_workers': 5,
     'db_filepath': '../db/propublica_officials.db',
     'db_table': 'officials_info',
+    'clean_db': False,
 }
 
 
@@ -20,7 +21,6 @@ API_PARAMS = {
     'API_KEY': 'f3qwGJui2ZkimFVYpwURI4ESpP0VEVvfNvoY4h38',
     'current_members_endpoint': 'https://api.propublica.org/congress/v1/'
                                 'members/{chamber}/{state}/current.json',
-
 }
 
 
@@ -49,15 +49,28 @@ def setup_table(conn):
     '''.format(tbl=DEFAULTS['db_table'])
 
     cursor = conn.cursor()
-
     cursor.execute(create_table)
+
+
+def clean_table(conn):
+    tbl = DEFAULTS['db_table']
+    print('Cleaning table %s' % tbl)
+    delete_table = '''
+        DELETE FROM {tbl};
+        VACUUM;
+    '''.format(tbl=tbl)
+
+    cursor = conn.cursor()
+    cursor.executescript(delete_table)
+    print('Successfully cleaned table %s' % tbl)
+    print()
 
 
 def retrieve_state_data(state, conn):
     print('Retrieving data for %s' % state)
     tbl = DEFAULTS['db_table']
     insert_table = '''
-        INSERT INTO {tbl} (
+        INSERT OR REPLACE INTO {tbl} (
             'id', 'state', 'name', 'first_name', 'middle_name',
             'last_name', 'role', 'gender', 'party', 'times_topics_url',
             'twitter_id', 'facebook_account', 'youtube_id', 'seniority',
@@ -70,8 +83,8 @@ def retrieve_state_data(state, conn):
             :next_election, :api_uri, :district, :at_large
         );
     '''.format(tbl=tbl)
-    print(insert_table)
     cursor = conn.cursor()
+    num_rows = 0
 
     for chamber in ['house', 'senate']:
         officials_endpoint = API_PARAMS['current_members_endpoint'].format(
@@ -90,14 +103,18 @@ def retrieve_state_data(state, conn):
             result['district'] = result.get('district')
             result['at_large'] = result.get('at_large')
             cursor.execute(insert_table, result)
+            num_rows += cursor.rowcount
+    print('Successfully retrieved %d rows for %s' % (num_rows, state))
+    print()
 
 
 def main():
-    states = parse_args()
-    print(states)
+    states, clean = parse_args()
 
     # Initialize db connection
     with sqlite3.connect(DEFAULTS['db_filepath']) as conn:
+        if clean:
+            clean_table(conn)
         setup_table(conn)
         for state in states:
             retrieve_state_data(state, conn)
@@ -109,14 +126,23 @@ def parse_args():
         description='Import data from ProPublica API')
     parser.add_argument('-s', '--states', metavar='state',
                         nargs='*', help='states to retrieve (two letter)')
+    parser.add_argument('-c', '--clean', action="store_true",
+                        help='remove all records from table'
+                             ' (specify states if you want to run script)')
     args = parser.parse_args()
-    states = DEFAULTS['states']
+    clean = DEFAULTS['clean_db']
+    if args.clean is not None:
+        clean = args.clean
+
+    # Only set states to default to GA if clean is not set
+    states = DEFAULTS['states'] if not clean else []
     if args.states:
         states = args.states
     if any(len(state) != 2 for state in states):
         print('Each state must be a two letter state code')
     states = [state.upper() for state in states]
-    return states
+
+    return states, clean
 
 
 if __name__ == '__main__':
