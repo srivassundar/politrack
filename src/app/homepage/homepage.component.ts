34fbfd3359cbdb056fb3c6b3bb8cec45c6f9f367
lindazhanghf <lindazhanghf@gmail.com>
2017-09-29 00:55:10 -0400

    Implement empty search check, need to fix UI/css

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Official } from './../official';
 import { OfficialService } from './../official.service';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'my-homepage',
@@ -12,15 +13,33 @@ import { Router } from '@angular/router';
 
 export class HomepageComponent implements OnInit {
   keyword: string;
+  invalid = false;
+
+  searchForm: NgForm;
 
   constructor(private router: Router,
               private officialService: OfficialService) { }
 
+
   ngOnInit(): void {
+    // this.keyword = "";
+  }
+
+  onClick(): void {
+    this.search();
   }
 
   onEnter(): void {
-    this.router.navigate(['/search', this.keyword]);
+    this.search();
+  }
+
+  search(): void {
+    if (this.keyword && this.keyword.length > 0) {
+      this.router.navigate(['/search', this.keyword]);
+    } else {
+      this.invalid = true;
+      console.log("set invalid");
+    }
   }
 
 }
